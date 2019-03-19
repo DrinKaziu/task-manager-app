ceb956b3eaf6659efb94dff51bb1289c04337a92
SnowSurfer <drinkaziu@gmail.com>
2019-03-19 17:18:39 -0400

    create Task endpoints

--- src/index.js
+++ src/index.js
@@ -66,6 +66,63 @@ app.delete('/users/:id', async (req, res) => {
   }
 });
 
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch(e) {
+    res.status(400).send(e);
+  }
+});
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch(e) {
+    res.status(500).send();
+  }
+});
+
+app.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id;
+  try { 
+    const task = await Task.findById(_id);
+    if (!task) return res.status(404).send()
+    res.send(task);
+  } catch(e) {
+    res.status(500).send();
+  } 
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const validUpdates = ['description', 'completed'];
+  const isValidUpdate = updates.every((update) => validUpdates.includes(update));
+
+  if (!isValidUpdate) return res.status(400).send({error: 'Please provide a valid item to update.'});
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true});
+    if (!task) return res.status(404).send();
+    res.send(task);
+  } catch(e) {
+    res.status(400).send(e);
+  }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) return res.status(404).send();
+    res.send(task);
+  } catch(e) {
+    res.status(500).send();
+  } 
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
 });
